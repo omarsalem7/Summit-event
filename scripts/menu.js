f6898b15de21203c822bfd2688a4c2bb2337f2c7
Omar <omarsalem0721@gmail.com>
2021-12-22 11:18:22 +0200

    Add dektop version, mobile menu and speakers with js

---
+++ scripts/menu.js
@@ -0,0 +1,27 @@
+// close and open btns selectors
+const openIcon = document.querySelector('#humberger');
+const closeIcon = document.querySelector('.closeIcon');
+
+const navbar = document.querySelector('.nav');
+const header = document.querySelector('.header');
+const main = document.querySelector('.main');
+
+const navbarOverlay = document.getElementById('navbar-overlay');
+
+openIcon.addEventListener('click', () => {
+  // creat layout
+  navbarOverlay.style.width = '100%';
+  // add blur in background
+  navbar.classList.add('blur');
+  header.classList.add('blur');
+  main.classList.add('blur');
+});
+
+closeIcon.addEventListener('click', () => {
+  //    remove the overlay
+  navbarOverlay.style.width = '0%';
+  //    remove blur from background
+  navbar.classList.remove('blur');
+  header.classList.remove('blur');
+  main.classList.remove('blur');
+});
---
+++ scripts/speakers.js
@@ -0,0 +1,73 @@
+const speakersData = [
+  {
+    id: 1,
+    name: 'Hossam Heikal',
+    topic: ' how to become influncer to convice people',
+    bio: 'Hossam is writer of the book “Al Wassya” s it’d sold more than 14,000 copies',
+    urlImage: './assets/HossamHeikal2.jpeg',
+  },
+  {
+    id: 2,
+    name: 'Dr. Khaled El-Nakib',
+    topic: 'How to get in to project Management',
+    bio: 'he is a project management consultant in Egypt and the Middle East.',
+    urlImage: './assets/khalid.jpg',
+  },
+  {
+    id: 3,
+    name: 'James Chursh',
+    topic: 'Become an Investable Entrepreneur',
+    bio: ' James has been featured on numerous industry blogs and podcasts.',
+    urlImage: './assets/james.jpg',
+  },
+  {
+    id: 4,
+    name: 'Omar Abdelrhaman',
+    topic: 'How professional branding can transform your Business',
+    bio: 'he is a Founder & CEO of Gateway English Community, but he’s known most for his YouTube channel.',
+    urlImage: './assets/omarabdo.jpg',
+  },
+  {
+    id: 5,
+    name: 'Hunter Bergman',
+    topic: 'Story Telling - Entrepreneurship In Nebraska',
+    bio: 'he serves as the Culture Coordinator for the UNL Center for Entrepreneurship., but he’s known most for his YouTube channel.',
+    urlImage: './assets/hunter.jpg',
+  },
+  {
+    id: 6,
+    name: 'Nadeem Barkat',
+    topic: 'Partnerships the wining coin',
+    bio: 'he is now taking over the commercial department at R2S Ecommerce Logistics leader in Egypt',
+    urlImage: './assets/Nadeem.jpg',
+  },
+];
+
+// create works project dynamically
+const speakersContainer = document.querySelector('.speakers__container');
+speakersData.forEach((speaker) => {
+  const speakerProject = document.createElement('div');
+  let speakerContent;
+
+  speakerContent = `   <div class="speakers__list__item">
+  <div class="speaker__list__item__img">
+      <img src=${speaker.urlImage} alt="${speaker.name}" />
+  </div>
+  <div class="speaker__list__item__content">
+      <h3>${speaker.name}</h3>
+      <p class="speaker__list__item__content__bio">
+         ${speaker.topic}
+      </p>
+      <div class="speakers__bio__line">
+
+      </div>
+      <p>
+      ${speaker.bio}
+      </p>
+  </div>
+</div>
+`;
+
+  speakerProject.innerHTML = speakerContent;
+  speakersContainer.appendChild(speakerProject);
+});
